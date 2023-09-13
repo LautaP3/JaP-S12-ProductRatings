45974ffc6f72e6cbfa08159271c443edcb63efff
LautaP3 <lautape04@gmail.com>
2023-09-12 22:21:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,59 @@ document.addEventListener("DOMContentLoaded", function (e) {
 })
 
 async function fetchData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    products = data
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        showProducts(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
 }
 
+function showProducts(products) {
+    const productsList = document.getElementById('products');
+    productsList.innerHTML = '';
 
-function showProducts() {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleString();
 
+    products.forEach(product => {
+        const listItem = document.createElement('div');
+        listItem.classList.add('list-group-item');
+
+        const cutTitle = cutString(product.title, 20);
+        const roundedRate = Math.round(product.rating.rate);
+
+        listItem.innerHTML = `
+            <h5>${cutTitle}</h5>
+            <div>${formattedDate} ${stars(roundedRate)}</div>
+        `;
+
+        productsList.appendChild(listItem);
+    });
 }
 
 function stars(cantidad) {
+    const starContainer = document.createElement('div');
+    starContainer.classList.add('stars');
 
+    for (let i = 1; i <= 5; i++) {
+        const star = document.createElement('span');
+        star.classList.add('fa', 'fa-star');
+
+        if (i <= cantidad) {
+            star.classList.add('checked'); 
+        }
+        starContainer.appendChild(star);
+    }
+
+    return starContainer.outerHTML;
 }
 
-function cutString(string) {
-
+function cutString(string, maxLength) {
+    if (string.length <= maxLength) {
+        return string;
+    } else {
+        return string.slice(0, maxLength) + '...';
+    }
 }
